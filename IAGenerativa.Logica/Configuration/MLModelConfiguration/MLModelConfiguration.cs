b48799846caaf456f3de88b272a28c29d2d28fbf
Sergio Gabriel Rubio <serrubio@publicisgroupe.net>
2025-06-21 02:43:27 -0300

    Se configuran variables del modelo generativo desde appsettings - Se quitan repositorios de mas

---
+++ IAGenerativa.Logica/Configuration/MLModelConfiguration/MLModelConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace IAGenerativaWeb.Models.Configuration.MLModelConfiguration
+{
+    public class MLModelConfiguration
+    {
+        public string AccessToken { get; set; }
+        public string ModelName { get; set; }
+        public string UrlBaseModel { get; set; }
+        public string BlobName { get; set; }
+    }
+}
